d42dc92fd4a790bf0569dcab653b7ab075c75511
maryamamin12 <rimshashehzadi79@gmail.com>
2026-07-31 18:57:56 +0500

    add

--- To_do/front-end/src/components/ParticlesBackground.jsx
+++ To_do/front-end/src/components/ParticlesBackground.jsx
@@ -1,63 +1,63 @@
-import { useCallback } from "react";
-import Particles from "@tsparticles/react";
-import { loadSlim } from "@tsparticles/slim";
+// import { useCallback } from "react";
+// import Particles from "@tsparticles/react";
+// import { loadSlim } from "@tsparticles/slim";
 
-const ParticlesBackground = () => {
+// const ParticlesBackground = () => {
 
-  const particlesInit = useCallback(async (engine) => {
-    await loadSlim(engine);
-  }, []);
+//   const particlesInit = useCallback(async (engine) => {
+//     await loadSlim(engine);
+//   }, []);
 
 
-  return (
-    <Particles
-      id="tsparticles"
-      init={particlesInit}
-      options={{
-        background: {
-          color: {
-            value: "transparent",
-          },
-        },
+//   return (
+//     <Particles
+//       id="tsparticles"
+//       init={particlesInit}
+//       options={{
+//         background: {
+//           color: {
+//             value: "transparent",
+//           },
+//         },
 
-        particles: {
-          number: {
-            value: 80,
-          },
+//         particles: {
+//           number: {
+//             value: 80,
+//           },
 
-          color: {
-            value: "#2E2EFF",
-          },
+//           color: {
+//             value: "#2E2EFF",
+//           },
 
-          links: {
-            enable: true,
-            color: "#2E2EFF",
-            opacity: 0.4,
-          },
+//           links: {
+//             enable: true,
+//             color: "#2E2EFF",
+//             opacity: 0.4,
+//           },
 
-          move: {
-            enable: true,
-            speed: 1,
-          },
+//           move: {
+//             enable: true,
+//             speed: 1,
+//           },
 
-          size: {
-            value: {
-              min: 1,
-              max: 4,
-            },
-          },
+//           size: {
+//             value: {
+//               min: 1,
+//               max: 4,
+//             },
+//           },
 
-          opacity: {
-            value: 0.5,
-          },
-        },
+//           opacity: {
+//             value: 0.5,
+//           },
+//         },
 
-        detectRetina: true,
-      }}
+//         detectRetina: true,
+//       }}
 
-      className="absolute inset-0"
-    />
-  );
-};
+//       className="absolute inset-0"
+//     />
+//   );
+// };
 
-export default ParticlesBackground;
+// export default ParticlesBackground;
